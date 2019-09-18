871933f5ad4ba17672626438af8e77ddaebac34b
adventurous-class-0665 <rngao97@gmail.com>
2019-09-18 05:07:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,12 @@ new_array = []
 return new_array
 end
 
+def reduce(array, starting_point = array[0])
+i = 0 
+value = starting_point
+  while i < array.length 
+  value = yield(value, array[i])
+  i += 1
+  end
+return new_array
+end
