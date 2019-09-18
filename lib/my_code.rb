e449e62a816834310c21716fae2f9f0c392f7567
adventurous-class-0665 <rngao97@gmail.com>
2019-09-18 04:59:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# Your Code Here
+# def map(array)
+#  array.map{yield}
+  
+# end
+ 
+# map(array){|x| x * -1}
+
+def make_sandwich(element1, element2)
+  yield("A #{element1} and #{element2} sandwich")
+end
+ 
+make_sandwich("asdf", "adsf") { |x| "#{x} on rye" }
