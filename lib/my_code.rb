43b8f02875fbcd930d9fc7ad48b351009444c58f
adventurous-class-0665 <rngao97@gmail.com>
2019-09-18 05:18:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,9 @@ end
 def reduce(array, starting_point = array[0])
 i = 0 
 value = starting_point
+if value == array[0]
   array.shift()
+end
   while i < array.length 
   value = yield(value, array[i])
   i += 1
